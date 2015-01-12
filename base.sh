bf95194fe1ed251acd06494b05f850f3b31ac0ec
Ben Widawsky <ben@bwidawsk.net>
2015-01-12 14:43:53 -0800

    Version 1. Probably bugs

---
+++ base.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+# Version history
+# 1.0 (1/11/15)
+
+VERSION="1.0"
+
+# Customize this to your own environments
+GL27_DATA_PATH=~/benchmarks/GLB27/data
+GL27_PATH=~/benchmarks/GLB27/buildES/binaries/GLBenchmark
+PIGLIT_PATH=~/intel-gfx/piglit
+VALLEY_PATH=~/benchmarks/Valley-1.1-rc1/bin
+SYNMARK_PATH=~/benchmarks/Synmark2-6.00/
+HEAVEN_PATH=~/benchmarks/Heaven-4.1-rc1/
+
+# Example ways to use this script:
+# Run GBM piglit with custom mesa:
+#	gbm.sh /foo/bar/mesa/lib PIGLIT [extra piglit args] results/dir
+# Run GBM piglit with system mesa:
+#	gbm.sh /usr/lib PIGLIT results/dir
+# Run GLX egypt benchmark with custom mesa:
+#	glx.sh /foo/bar/mesa/lib EGYPT
+# Run GLX with menu and custom mesa:
+#	glx.sh /foo/bar/mesa/lib
+# Run GLX with menu and system mesa:
+#	base.sh
+
+function heaven() {
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./bin
+	./bin/heaven_x64 -video_app opengl -data_path ../ -sound_app null \
+		-engine_config ../data/heaven_4.0.cfg -video_multisample 0 \
+		-system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 \
+		-extern_define PHORONIX,RELEASE
+}
+
+function valley() {
+	./valley_x64 -project_name Valley -data_path ../ -engine_config \
+		../data/valley_1.1.cfg -system_script valley/unigine.cpp \
+		-sound_app null -video_app opengl -video_multisample 0 \
+		-video_mode -1 -video_fullscreen 1 -extern_define PHORONIX,RELEASE
+}
+
+function init() {
+	xset -dpms; xset s off
+	xscreen=$(which xscreensaver-command 2>/dev/null)
+	[[ $? -eq 0 ]] && eval "${xscreen} -deactivate >/dev/null 2>&1"
+}
+
+function env_sanitize() {
+	unset LD_LIBRARY_PATH
+	unset LIBGL_DRIVERS_PATH
+	unset LD_LIBRARY_PATH
+	unset PIGLIT_PLATFORM
+	unset vblank_mode
+	unset EGL_PLATFORM
+	unset EGL_DRIVERS_PATH
+	unset DISPLAY
+}
+
+function get_dimensions() {
+	if hash xdpyinfo 2>/dev/null; then
+		read RES_X RES_Y <<< $(xdpyinfo | grep dimensions | \
+			awk '{print $2}' | awk -Fx '{print $1, $2}')
+	else
+		RES_X=1920
+		RES_Y=1080
+	fi
+	export RES_X
+	export RES_Y
+}
+
+function glx_env() {
+	env_sanitize
+	export vblank_mode=0
+	export LD_LIBRARY_PATH=${1}
+	export LIBGL_DRIVERS_PATH=${1}/dri
+	export DISPLAY=:0
+	[[ -z $RES_X ]] && get_dimensions
+}
+
+function gbm_env() {
+	env_sanitize
+	export vblank_mode=0
+	export LD_LIBRARY_PATH=${1}
+	export LIBGL_DRIVERS_PATH=${1}/dri
+	export EGL_DRIVERS_PATH=${LIBGL_DRIVERS_PATH}
+	export EGL_PLATFORM=drm
+	export PIGLIT_PLATFORM=gbm
+}
+
+SCRIPT_PATH=$(dirname $BASH_SOURCE)
+declare -A TESTS
+TESTS[XONOTIC]='
+xonotic-glx -benchmark demos/the-big-keybench 2>&1 | \
+	egrep -e "[0-9]+ frames" | awk "{print \$5}"'
+
+TESTS[QA_XONOTIC]='
+xonotic-glx -nohome -benchmark demos/the-big-keybench \
+	+r_glsl 1 +vid_width $RES_X +vid_height $RES_Y +exec effects-low.cfg'
+
+TESTS[WARSOW]='
+warsow +exec high+.cfg +timedemo 1 +seta vid_mode -2 +seta vid_fullscreen 1 \
+	+set timedemo 1 +exec profiles/high+.cfg +seta cg_showspeed 1 \
+	+demo benchsow.wdz20 \
+	+next "quit" 2> /dev/null 2>&1 | grep frames | awk "{print \$5}"'
+
+TESTS[TREX]='
+$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
+	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
+	-t GLB27_TRex_C24Z16_FixedTimeStep | \
+	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
+
+TESTS[TREX_O]='
+$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
+	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
+	-t GLB27_TRex_C24Z16_FixedTimeStep_Offscreen | \
+	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
+
+TESTS[EGYPT]='
+$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
+	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
+	-t GLB27_EgyptHD_inherited_C24Z16_FixedTime | \
+	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
+
+TESTS[EGYPT_O]='
+$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
+	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
+	-t GLB27_EgyptHD_inherited_C24Z16_FixedTime_Offscreen | \
+	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
+
+TESTS[VALLEY]='cd $VALLEY_PATH ; valley | grep -i fps | awk "{print \$2}"'
+TESTS[HEAVEN]='cd $HEAVEN_PATH ; heaven | grep -i fps  | awk "{print \$2}"'
+
+#Be careful. I sed this, so newlines don't work easily
+TESTS[SYNMARK]='
+cd $SYNMARK_PATH ; $SYNMARK_PATH/synmark2 TESTNAMEHERE | grep FPS | awk "{print \$2}"'
+
+TESTS[FUR]='
+gputest /fullscreen /width=$RES_X /height=$RES_Y \
+	/benchmark /benchmark_duration_ms=10000 \
+	/print_score /no_scorebox \
+	/test=fur | \
+	grep FPS |awk "{gsub(/\)\$/,\"\"); print \$5}"'
+
+TESTS[PLOT3D]='
+gputest /fullscreen /width=$RES_X /height=$RES_Y \
+	/benchmark /benchmark_duration_ms=10000 \
+	/print_score /no_scorebox \
+	/test=plot3d | \
+	grep Score |  awk "{print \$2}"'
+
+TESTS[TRIANGLE]='
+gputest /fullscreen /width=$RES_X /height=$RES_Y \
+	/benchmark /benchmark_duration_ms=10000 \
+	/print_score /no_scorebox \
+	/test=triangle |
+	grep Score |  awk "{print \$2}"'
+
+TESTS[PIGLIT]='cd $PIGLIT_PATH ; ./piglit-run.py -x glean -x glx quick'
+TESTS[NOP]='echo 10' #Sanity check
+
+# If sourced from another script, just leave
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return
+
+if [[ $# && "$1" = "-v" ]] ; then
+	echo version: $VERSION
+	exit 0
+fi
+
+env_sanitize
+
+# IF our script name was gbm.sh, setup the GBM environment. If it was named
+# glx, then do the usual thing. Default to local mesa install
+script_name=`basename $0`
+if [[ $script_name = "gbm.sh" ]] ; then
+	gbm_env $1
+	shift
+elif [[ $script_name = "glx.sh" ]] ; then
+	glx_env $1
+	shift
+fi
+
+init
+
+if [[ $# -eq 0 ]];  then
+	prompt="Pick an option:"
+
+	PS3="Select test (just hit ctrl+c to exit)"
+	select test in ${!TESTS[*]} "Exit"; do
+	    case "$REPLY" in
+	    *) eval "${TESTS[$test]}";;
+	    esac
+	done
+else
+	if [[ "$1" = "SYNMARK" ]] ; then
+		syn_test=$2
+		cmd=${TESTS[$1]/TESTNAMEHERE/$syn_test}
+		shift
+		shift
+		eval $cmd
+	else
+		index=$1
+		shift
+		eval "${TESTS[$index]} $*"
+	fi
+fi
---
+++ run_shuffled_benchmarks.sh
@@ -0,0 +1,240 @@
+#!/usr/bin/env bash
+
+# Version history:
+# 1.0 (1/11/15): Things seem to work.
+
+
+set -u #undeclared variables are not okay
+
+# This runner is built on top of the core runner provided by base.sh. That
+# runner has some variables to set for the paths of various benchmark data.
+
+# In order to use this runner, place a symlink named glx.sh which links to
+# base.sh. That glx.sh must reside:
+GLX_RUNNER="~/scripts/tests/glx.sh"
+
+# Next, the script expects the results of `make DESTDIR=foo install` for all mesa
+# builds that are to be tested here:
+MESA_LIBS="$HOME/mesa-test-dir/*"
+
+#http://mywiki.wooledge.org/BashFAQ/026
+# Returns random number from 0 to ($1-1) in global var 'r'.
+# Bash syntax.
+function rand() {
+	local max=$((32768 / $1 * $1))
+	while (( (r=$RANDOM) >= max )); do :; done
+	r=$(( r % $1 ))
+}
+
+function shuffle() {
+   local i tmp size max
+
+   # $RANDOM % (i+1) is biased because of the limited range of $RANDOM
+   # Compensate by using a range which is a multiple of the array size.
+   size=${#TEST_LIST[*]}
+   max=$(( 32768 / size * size ))
+
+   for ((i=size-1; i>0; i--)); do
+      rand $size
+      tmp=${TEST_LIST[i]} TEST_LIST[i]=${TEST_LIST[r]} TEST_LIST[r]=$tmp
+   done
+}
+
+function populate_test_list() {
+	local RUNS=$1
+	local ndx=0
+	for ((i=1;i<=RUNS;i++)); do
+		for mesa in $MESA_LIBS; do
+			output=$(basename $mesa)
+
+			if [[ "$GLBENCH" = "true" ]] ; then
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib EGYPT >> bench_egypt_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib EGYPT_O >> bench_egyptoff_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib TREX >> bench_trex_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib TREX_O >> bench_trexoff_${output}"
+			fi
+
+			if [[ "$COMMUNITY" = "true" ]] ; then
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib WARSOW >> bench_warsow_${output}"
+			fi
+
+			if [[ "$GPUTEST" = "true" ]] ; then
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib FUR >> bench_fur_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib PLOT3D >> bench_plot3d_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib TRIANGLE >> bench_triangle_${output}"
+			fi
+
+			if [[ "$SYNMARK" = "true" ]] ; then
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglFillPixel >> bench_OglFillPixel_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglFillTexMulti >> bench_OglFillTexMulti_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglFillTexSingle >> bench_OglFillTexSingle_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTexFilterAniso >> bench_OglTexFilterAniso_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTexFilterTri >> bench_OglTexFilterTri_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTexMem128 >> bench_OglTexMem128_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTexMem512 >> bench_OglTexMem512_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglGeomPoint >> bench_OglGeomPoint_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglGeomTriList >> bench_OglGeomTriList_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglGeomTriStrip >> bench_OglGeomTriStrip_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglZBuffer >> bench_OglZBuffer_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch0 >> bench_OglBatch0_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch1 >> bench_OglBatch1_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch2 >> bench_OglBatch2_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch3 >> bench_OglBatch3_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch4 >> bench_OglBatch4_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch5 >> bench_OglBatch5_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch6 >> bench_OglBatch6_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglBatch7 >> bench_OglBatch7_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglVSDiffuse1 >> bench_OglVSDiffuse1_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglVSDiffuse8 >> bench_OglVSDiffuse8_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglVSTangent >> bench_OglVSTangent_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglVSInstancing >> bench_OglVSInstancing_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglPSPhong >> bench_OglPSPhong_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglPSBump2 >> bench_OglPSBump2_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglPSBump8 >> bench_OglPSBump8_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglPSPom >> bench_OglPSPom_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglShMapPcf >> bench_OglShMapPcf_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglShMapVsm >> bench_OglShMapVsm_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglCSCloth >> bench_OglCSCloth_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglOclCloth >> bench_OglOclCloth_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglCSDof >> bench_OglCSDof_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglOclDof >> bench_OglOclDof_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglDeferred >> bench_OglDeferred_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglDeferredAA >> bench_OglDeferredAA_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglHdrBloom >> bench_OglHdrBloom_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglMultithread >> bench_OglMultithread_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTerrainPanInst >> bench_OglTerrainPanInst_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTerrainFlyInst >> bench_OglTerrainFlyInst_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTerrainPanTess >> bench_OglTerrainPanTess_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglTerrainFlyTess >> bench_OglTerrainFlyTess_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglDrvState >> bench_OglDrvState_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglDrvShComp >> bench_OglDrvShComp_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglDrvRes >> bench_OglDrvRes_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib SYNMARK OglDrvCtx >> bench_OglDrvCtx_${output}"
+			fi
+
+			if [[ "$UNIGINE" = "true" ]] ; then
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib VALLEY >> bench_valley_${output}"
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib HEAVEN >> bench_heaven_${output}"
+			fi
+
+			if [[ "$COMMUNITY" = "true" && "$LONG" = "true" ]] ; then
+				# Xonotic takes more than 8 minutes on Braswell, so
+				# just don't run it unless the user is a super
+				# masochist.
+				TEST_LIST[((ndx++))]="${GLX_RUNNER} ${mesa}/usr/local/lib XONOTIC >> bench_xonotic_${output}"
+			fi
+		done
+	done
+}
+
+if [[ $# -ne 0 ]] ; then
+	# If the user specified any arguments, set everything to false
+	GLBENCH="false"
+	SYNMARK="false"
+	UNIGINE="false"
+	COMMUNITY="false"
+	LONG="false"
+	GPUTEST="false"
+	ITERATIONS=0
+	POST_DELETE="false"
+else
+	# Otherwise, use all the defaults
+	GLBENCH="true"
+	SYNMARK="true"
+	UNIGINE="false"
+	COMMUNITY="false"
+	LONG="false"
+	GPUTEST="true"
+	ITERATIONS=5
+	POST_DELETE="true"
+fi
+
+DRY_RUN="false"
+VERBOSE="false"
+
+function usage() {
+	local script_name=$1
+	echo Usage: $script_name [-AgsucGdlhnv] [-i iterations] 1>&2
+	echo -e "\t-A: Run all tests"
+	echo -e "\t-g: Run glbench tests"
+	echo -e "\t-s: Run synmark tests"
+	echo -e "\t-c: Run community tests"
+	echo -e "\t-G: Run gputest tests"
+	echo -e "\t-l: Run long (temporally) tests"
+	echo -e "\t-d: Delete empty files (failures)"
+	echo -e "\t-i iter: Number of iterations to run tests"
+	echo -e "\t-h: this message"
+	echo -e "\t-v: verbose"
+	echo -e "\tn: Dry run"
+	echo -e "\tno args: $script_name -gsGcd -i 5"
+	echo "Specifying any arguments will set all things to false"
+}
+
+while getopts "AgsucGdlhni:v" opt; do
+	case "$opt" in
+		A)
+			GLBENCH="true"
+			SYNMARK="true"
+			UNIGINE="true"
+			COMMUNITY="true"
+			LONG="true"
+			GPUTEST="true"
+			# The user probably wants defaults also
+			ITERATIONS=5
+			POST_DELETE="true"
+			;;
+		g) GLBENCH="true" ;;
+		s) SYNMARK="true" ;;
+		G) GPUTEST="true" ;;
+		u) UNIGINE="true" ;;
+		c) COMMUNITY="true" ;;
+		l) LONG="true" ;;
+		d) POST_DELETE="true" ;;
+		i) ITERATIONS=$OPTARG ;;
+		n) DRY_RUN="true" ;;
+		v) VERBOSE="true" ;;
+		h|\?|*)
+			usage $(basename $0) >&2
+			exit 0
+			;;
+	esac
+done
+shift "$((OPTIND-1))" # Shift off the options and optional --.
+
+# Skip any null cases
+[[ $ITERATIONS < 1 ]] && echo "0 iterations specified" && exit 0
+[[ "$GLBENCH" = "false" ]] && [[ "$SYNMARK" = "false" ]] &&
+[[ "$UNIGINE" = "false" ]] && [[ "$COMMUNITY" = "false" ]] &&
+[[ "$GPUTEST" = "false" ]] && echo "No tests specified" && exit 0
+
+populate_test_list ${ITERATIONS}
+shuffle
+
+# Attempt to get resolution before any possible modesets
+get_dimensions #set RES_X RES_Y
+
+
+for (( i = 0 ; i < ${#TEST_LIST[*]} ; i++ )) do
+	before=$(date)
+	test_name=$(echo ${TEST_LIST[i]} | awk '{print $NF}')
+
+	if [[ "$VERBOSE" = "true" ]] ; then
+		echo -n "Starting $test_name $(date +'%b %d %T') ..."
+	else
+		echo -ne "[$(($i+1)) / ${#TEST_LIST[*]}] ($test_name) \033[0K\r"
+	fi
+
+	if [[ "$DRY_RUN" = "true" ]] ; then
+		echo "${TEST_LIST[i]} > /dev/null 2>&1"
+	else
+		eval ${TEST_LIST[i]} > /dev/null 2>&1
+	fi
+
+	elapsed=$(date -d @$(( $(date -d "now" +%s) - $(date -d "$before" +%s))) -u +'%H:%M:%S')
+	[[ "$VERBOSE" = "true" ]] && echo "elapsed: $elapsed"
+done
+
+echo
+
+#remove empty files
+[[ "$POST_DELETE" = "true" ]] && rm -f $(find . -empty)
