2ea81f1c2e9c396b6a5f54cb9f2421ea16b04c0e
Ben Widawsky <ben@bwidawsk.net>
2015-07-02 19:22:52 -0700

    gfxbench3: explicitly specify offscreen dimensions
    I did this for glb2.7, not sure why I never bothered for gfxb3.

--- base.sh
+++ base.sh
@@ -82,7 +82,9 @@ function jordanatic() {
 
 function gfxbench30() {
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-	./mainapp -w $RES_X -h $RES_Y -t ${1} -fullscreen 1 | \
+	./mainapp -w $RES_X -h $RES_Y \
+		-offscreen_width $RES_X -offscreen_height $RES_Y \
+		-t ${1} -fullscreen 1 | \
 		grep score | awk -F"[ ,]" "{printf \"%.3f\\n\", \$5}"
 }
 
