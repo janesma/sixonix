214629f015ff613eb2d3bb2f666da602bc0399a0
Ben Widawsky <ben@bwidawsk.net>
2015-07-10 08:00:11 -0700

    Add system info dump to piglit run

--- run_piglit.sh
+++ run_piglit.sh
@@ -7,6 +7,8 @@ MESA_LIBS="$HOME/mesa-test-dir/*"
 
 source ${GBM_RUNNER}
 
+dump_system_info "$MESA_LIBS" execution.log
+
 for mesa in $MESA_LIBS; do
 	output=$(basename $mesa)
 	${GBM_RUNNER} ${mesa}/usr/local/lib PIGLIT ${PWD}/piglit_${output}
