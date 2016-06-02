c43640861495871f64b5f087d34c2c510b98aadd
Ben Widawsky <ben@bwidawsk.net>
2016-06-02 10:53:54 -0700

    glbench2.7: make a common fuction

--- conf.d/glbench27.sh
+++ conf.d/glbench27.sh
@@ -2,26 +2,14 @@ GLB27_BASE=$BENCHDIR/_deprecated_/GLB27/
 GL27_DATA_PATH=$GLB27_BASE/data
 GL27_PATH=$GLB27_BASE/buildES/binaries/GLBenchmark
 
-TESTS[TREX_2.7]='
-$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
-	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
-	-t GLB27_TRex_C24Z16_FixedTimeStep | \
-	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
+function gfxbench27() {
+	$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
+		-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
+		-t ${1} | \
+		grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"
+}
 
-TESTS[TREX_O_2.7]='
-$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
-	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
-	-t GLB27_TRex_C24Z16_FixedTimeStep_Offscreen | \
-	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
-
-TESTS[EGYPT]='
-$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
-	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
-	-t GLB27_EgyptHD_inherited_C24Z16_FixedTime | \
-	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
-
-TESTS[EGYPT_O]='
-$GL27_PATH -data $GL27_DATA_PATH -skip_load_frames \
-	-w $RES_X -h $RES_Y -ow $RES_X -oh $RES_Y \
-	-t GLB27_EgyptHD_inherited_C24Z16_FixedTime_Offscreen | \
-	grep fps | awk -F "[()]" "{print \$2}" | awk "{print \$1}"'
+TESTS[TREX_2.7]='gfxbench27 GLB27_TRex_C24Z16_FixedTimeStep'
+TESTS[TREX_O_2.7]='gfxbench27 GLB27_TRex_C24Z16_FixedTimeStep_Offscreen'
+TESTS[EGYPT]='gfxbench27 GLB27_EgyptHD_inherited_C24Z16_FixedTime'
+TESTS[EGYPT_O]='gfxbench27 GLB27_EgyptHD_inherited_C24Z16_FixedTime_Offscreen'
