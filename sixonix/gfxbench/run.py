229d8311217b906471361e5bd720cccf9c2ec2ee
Jason Ekstrand <jason.ekstrand@intel.com>
2017-09-12 08:00:11 -0700

    gfxbench: Rework to use the new config parser

--- sixonix/gfxbench/run.py
+++ sixonix/gfxbench/run.py
@@ -9,29 +9,19 @@ import shutil
 import subprocess
 import sys
 
+from .. import config
+
 def run(test, args=None):
     """test gfxbench"""
-    conf_file = path.join(path.dirname(__file__), "conf.json")
-    assert path.exists(conf_file)
-    conf = json.load(open(conf_file))
-    platform = "linux"
-    if "win" in sys.platform:
-        platform = "windows"
-    bench_dir = path.join(SIXONIX_DIR, "benchmarks", "gfxbench", platform)
-    executable_path = path.join(bench_dir, conf[platform]["executable"])
-    base_dir = path.abspath(executable_path + "/..")
-    width = "1920"
-    height = "1080"
-    fullscreen = True
-    if args:
-        width = args.width
-        height = args.height
-        fullscreen = (args.fullscreen == "true")
+    conf = config.get_config_for_module("gfxbench")
+    assert len(conf.executables) == 1
+    executable_path = path.join(conf.benchmark_path, conf.executables[0])
 
-    if platform == "windows":
+    base_dir = path.dirname(executable_path)
+    if conf.platform == "windows":
         # on windows, the base directory is one level higher.
-        base_dir = path.abspath(base_dir + "/..")
-    results_dir = base_dir + "/results"
+        base_dir = path.dirname(base_dir)
+    results_dir = os.path.join(base_dir, "results")
     tests = {"manhattan" : "gl_manhattan31",
              "manhattan_o" : "gl_manhattan31_off",
              "car_chase" : "gl_4",
@@ -57,12 +47,12 @@ def run(test, args=None):
            "-b", base_dir,
            "-t", tests[test],
            "--gfx", "glfw"]
-    cmd += ["--ei", "-offscreen_width=" + width,
-            "--ei", "-offscreen_height=" + height]
-    if fullscreen:
+    cmd += ["--ei", "-offscreen_width=" + args.width,
+            "--ei", "-offscreen_height=" + args.height]
+    if args.fullscreen == "true":
         cmd += ["--ei", "-fullscreen=1"]
     else:
-        cmd += ['-w', width, '-h', height]
+        cmd += ['-w', args.width, '-h', args.height]
     with subprocess.Popen(cmd,
                           stderr=subprocess.PIPE,
                           stdout=open(os.devnull, "w"),
@@ -77,8 +67,3 @@ def run(test, args=None):
     print(score["results"][0]["gfx_result"]["fps"])
 
     shutil.rmtree(results_dir)
-
-if __name__ == "__main__":
-    run(sys.argv[1].lower())
-else:
-    SIXONIX_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
