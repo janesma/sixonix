8397dd7b019eb4d00b92ad587b358dd62ff67718
Jason Ekstrand <jason.ekstrand@intel.com>
2017-09-12 21:07:38 -0700

    sixonix: Pull benchmark listing into core

--- run_benchmark.py
+++ run_benchmark.py
@@ -5,20 +5,7 @@
 import argparse
 import os.path as path
 import sys
-import sixonix.gfxbench
-import sixonix.gputest
-import sixonix.synmark
-import sixonix.unigine
-
-SUITES = {}
-for name in sixonix.gfxbench.BENCHMARKS:
-    SUITES[name] = "gfxbench"
-for name in sixonix.gputest.BENCHMARKS:
-    SUITES[name] = "gputest"
-for name in sixonix.synmark.BENCHMARKS:
-    SUITES[name] = "synmark"
-for name in sixonix.unigine.BENCHMARKS:
-    SUITES[name] = "unigine"
+import sixonix
 
 cmd = sys.argv[0]
 parser = argparse.ArgumentParser(description="sixoxix runner")
@@ -34,26 +21,12 @@ args = parser.parse_args(sys.argv[1:])
 SIXONIX_DIR = path.abspath(path.join(path.dirname(cmd), ".."))
 
 BENCH = args.benchmark
-if BENCH not in SUITES:
-    BENCH = BENCH.lower()
-if BENCH not in SUITES:
-    print("ERROR: unknown benchmark.  Choose one of: " +
-          " ".join(SUITES.keys()))
+if BENCH not in sixonix.BENCHMARKS:
+    print("ERROR: unknown benchmark.  Choose one of:")
+    for test in sixonix.BENCHMARKS:
+        print("    " + test)
     sys.exit(-1)
 
 sys.path.append(path.join(path.dirname(path.abspath(sys.argv[0])), ".."))
 
-MODULE = None
-if SUITES[BENCH] == "unigine":
-    MODULE = sixonix.unigine
-elif SUITES[BENCH] == "gfxbench":
-    MODULE = sixonix.gfxbench
-elif SUITES[BENCH] == "gputest":
-    MODULE = sixonix.gputest
-elif SUITES[BENCH] == "synmark":
-    MODULE = sixonix.synmark
-else:
-    assert False
-
-MODULE.install()
-MODULE.run(BENCH, args)
+sixonix.run(BENCH, args)
--- sixonix/__init__.py
+++ sixonix/__init__.py
@@ -1,4 +1,30 @@
+import os
 import os.path
+import importlib
 
 SIXONIX_PATH = os.path.join(os.path.dirname(__file__))
 BENCHMARKS_PATH = os.path.join(SIXONIX_PATH, "..", "benchmarks")
+
+SUITES = {}
+BENCHMARKS = []
+
+for name in os.listdir(os.path.dirname(__file__)):
+    try:
+        mod = importlib.import_module(".".join([__package__, name]))
+        if "run" in dir(mod):
+            SUITES[name] = mod
+            BENCHMARKS += [".".join([name, test]) for test in mod.BENCHMARKS]
+    except ModuleNotFoundError:
+        continue
+
+BENCHMARKS.sort()
+
+from . import install as _install
+
+def run(test, args, install = True):
+    suite = test.split(".")[0]
+    test = test.split(".")[1]
+    if install:
+        SUITES[suite].install()
+
+    return SUITES[suite].run(test, args)
