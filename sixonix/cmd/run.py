d454f4b9e483ec0f15a76d8a8053829526209aa0
Jason Ekstrand <jason.ekstrand@intel.com>
2017-10-25 11:56:05 -0700

    run: Add a --mesa-path flag

--- sixonix/cmd/run.py
+++ sixonix/cmd/run.py
@@ -1,3 +1,4 @@
+import os
 import sixonix
 import sys
 
@@ -10,11 +11,18 @@ def run_benchmark(args):
             print("    " + test)
         sys.exit(-1)
 
-    fps = sixonix.run(args.benchmark, args)
+    env = os.environ
+    if args.mesa_path is not None:
+        env = env.copy()
+        sixonix.mesa.MesaPrefix(args.mesa_path).update_env(env)
+
+    fps = sixonix.run(args.benchmark, args, env)
     print(fps)
 
 def register_cmd(subparsers):
     parser = subparsers.add_parser('run', help='run a single benchmark',
                                    parents=[sixonix.run_argparse])
     parser.add_argument('benchmark', help="benchmark to run")
+    parser.add_argument('--mesa-path', type=str, default=None,
+                        help='path to the mesa build to use')
     parser.set_defaults(func=run_benchmark)
